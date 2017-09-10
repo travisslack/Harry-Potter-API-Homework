ef81b1fb61c7985c591d7ee4d4612d54594fa051
Travis Grimmond <travisslack1994@outlook.com>
2017-09-10 22:13:02 +0100

    render made

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', funciton() {
+window.addEventListener('load', function () {
   var url = 'http://hp-api.herokuapp.com/api/characters';
 
   makeRequest(url, function() {
@@ -6,5 +6,30 @@ window.addEventListener('load', funciton() {
     var jsonString = this.responseText;
     var wizards = JSON.parse(jsonString);
     render(wizards);
-  })
-})
+  });
+});
+
+var makeRequest = function (url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.addEventListener('load', callback);
+  xhr.send();
+}
+
+var render = function (wizards) {
+  var storedWizard = localStorage.getItem('selectedWizard');
+  var wizardToDisplay = null;
+
+  if (storedWizard) {
+    wizardToDisplay = JSON.parse(storedWizard);
+    var select = document.querySelector('#wizards');
+    select.selectedIndex = wizardToDisplay.index;
+  }
+  else {
+    wizardToDisplay = wizards[0];
+  }
+
+  populateSelect(wizards);
+  updateInfo(wizardToDisplay);
+}
+
